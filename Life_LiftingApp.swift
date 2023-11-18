c682bb7abf67571eaecbefa477cf2e9d0261430d
lm-2001 <55418637+lm-2001@users.noreply.github.com>
2023-11-17 21:41:23 -0500

    Add files via upload

---
+++ Life_LiftingApp.swift
@@ -0,0 +1,19 @@
+//
+//  Life_LiftingApp.swift
+//  Life Lifting
+//
+//  Created by Lyah Morales.
+//
+
+import SwiftUI
+
+@main
+struct Life_LiftingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+
+        }
+    }
+}
