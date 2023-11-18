50adf30458dbb1d847881abd34577748537e8cd8
lm-2001 <55418637+lm-2001@users.noreply.github.com>
2023-11-17 21:39:37 -0500

    Add files via upload

---
+++ InfoView.swift
@@ -0,0 +1,30 @@
+//
+//  InfoView.swift
+//  Life Lifting
+//
+//  Created by Lyah Morales
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    //Variables
+    
+    var body: some View {
+        
+        NavigationView{
+            
+            //Add Code
+            
+        }
+        
+    }
+    
+    
+    struct InfoView_Previews: PreviewProvider {
+        static var previews: some View {
+            InfoView()
+        }
+    }
+}
